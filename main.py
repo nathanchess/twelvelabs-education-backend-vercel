73526bb9eb27b30bf164654f5e69037a807cb459
Nathan Che <nanache21@gmail.com>
2025-09-09 17:06:28 -0700

    origin local host domain added

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ origins = [
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins, 
-    allow_credentials=True,
+    allow_origins=['*'], 
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
