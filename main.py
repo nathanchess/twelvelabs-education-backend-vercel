f10088fdd4426184c06460a0217eb3100eda47b7
Nathan Che <nanache21@gmail.com>
2025-09-09 17:02:28 -0700

    origin local host domain added

--- main.py
+++ main.py
@@ -17,10 +17,14 @@ from fastapi.staticfiles import StaticFiles
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+origins = [
+    "http://localhost:3000",
+]
+
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
